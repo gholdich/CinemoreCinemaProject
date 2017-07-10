e9269fef45278451b577927ce4f12d6b39e44d29
rosieoleary <rosie651@btinternet.com>
2017-07-10 17:13:40 +0100

    logo link

--- client/src/components/Brand.jsx
+++ client/src/components/Brand.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Brand extends Component {
 	render() {
 		return(
 			<div className="logo-container">
-				<img id="logo" src="/images/logo.png" alt="Logo" />
+				<Link to ="/">
+					<img id="logo" src="/images/logo.png" alt="Logo" />
+				</Link>
 			</div>
 		);
 	}
