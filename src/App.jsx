54a0c9e542842d72c726106850a0f8179d619a27
rosieoleary <rosie651@btinternet.com>
2017-07-10 09:42:58 +0100

    first completed App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,21 @@
-//App.jsx file
+import React from 'react';
+import Header from '..components/Header';
+import Footer from '..components/Footer';
+import BookNow from '..components/BookNow';
+
+
+export default class App from React.Component{
+	render(){
+		return(
+			<div>
+				<Header/>
+				<main>
+					{this.properties.children}
+				</main>
+				<BookNow/>
+				<Footer>
+			</div>
+		
+		);
+	}
+}
