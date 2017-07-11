d16e16bc473b98f84b34499a9c056b6a26edac7e
Mallett94 <nmallett94@gmail.com>
2017-07-11 11:45:09 +0100

    nath small edits. filmblock no a wip

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import BookNow from './components/BookNow';
 import MainRoutes from './MainRoutes';
 
 
--- client/src/pages/listing/FilmBlock.jsx
+++ client/src/pages/listing/FilmBlock.jsx
@@ -3,8 +3,9 @@ import React , { Component } from 'react';
 export class filmBlock extends Component {
 	render() {
 		return(
-			<div >
-			
+			<div className="mainDiv" >
+				<div id="posterDiv" ><img id="poster" src="this.props.source" /></div>//work in progress
+				<span id="film description/info" >this.props.filmInfo</span>//work in progress
 			</div>
 		);
 	}
