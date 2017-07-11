c7c36cd20b4a402e0c450cb6cbb03921bc3a55ff
James Charlesworth <jcharlesworthwork@gmail.com>
2017-07-11 11:25:59 +0100

    added confirmation page and paypal integration to booking

---
+++ client/src/pages/confirmation/Confirmation.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class Confirmation extends Component{
+	render(){
+		return(
+			<div>
+				<h2>Payment Confirmed</h2>
+				<p>Enjoy your film. Idiot!</p>
+			</div>
+		);
+	}
+}
