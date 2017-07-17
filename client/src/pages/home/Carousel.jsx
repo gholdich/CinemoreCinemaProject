3342021a07acafda5c1f746fb2cb6ee81908a57f
Mallett94 <nmallett94@gmail.com>
2017-07-17 11:31:15 +0100

    Carousel auoscrolls. removed need for nuka-carousel-autoscroll and react-addons-shallowcompare so can uninstall those

--- client/src/pages/home/Carousel.jsx
+++ client/src/pages/home/Carousel.jsx
@@ -1,22 +1,16 @@
 import React, { Component } from 'react';
 import NukaCarousel from 'nuka-carousel';
-import { NukaDecorate } from 'nuka-carousel-autoscroll';
+//import { NukaDecorate } from 'nuka-carousel-autoscroll';
 
 //const NukaDecorated = NukaDecorate(NukaCarousel);
 
 export default class Slideshow extends Component {
-/*a(){
-	return setInterval(function() {
-		return(
-				<img className="carouselPoster" src={"/images/carousel/"+postername}/>
-			);
-	},	3000);
-}*/
+
 	mixins: [NukaCarousel.ControllerMixin]
 	render() {
   	return (
-			//{this.a()}
-	   	<NukaCarousel>
+
+	   	<NukaCarousel wrapAround = {true} autoplay = {true} autoplayInterval = {2500}>
 	      <img className="carouselPoster" src={"/images/carousel/wonderwoman.jpg"}/>
 	      <img className="carouselPoster" src={"/images/carousel/War-For-The-Planet-Of-The-Apes.png"}/>
 	      <img className="carouselPoster" src={"/images/carousel/Spider-Man-Homecoming.jpg"}/>
@@ -28,5 +22,9 @@ export default class Slideshow extends Component {
   	);
 	}
 }
-
-//CinemoreCinemaProject/client/public
+/*
+slidesToScroll: React.PropTypes.oneOfType([
+  React.PropTypes.number,
+  React.PropTypes.oneOf(['auto'])
+])
+*/
