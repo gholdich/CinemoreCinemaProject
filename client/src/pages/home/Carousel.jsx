fdd19da9acff8f48a98dccc37b8985024ba25a05
Mallett94 <nmallett94@gmail.com>
2017-07-17 10:31:52 +0100

    carousel working manually, doesnt change automatically yet. New ode module added so run npm install before you try to run new version

--- client/src/pages/home/Carousel.jsx
+++ client/src/pages/home/Carousel.jsx
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import NukaCarousel from 'nuka-carousel';
+import { NukaDecorate } from 'nuka-carousel-autoscroll';
 
-export default class Carousel extends Component {
-	render() {
+//const NukaDecorated = NukaDecorate(NukaCarousel);
+
+export default class Slideshow extends Component {
+/*a(){
+	return setInterval(function() {
 		return(
-		<div >
-			Pretty posters please
-		</div>
-		);
+				<img className="carouselPoster" src={"/images/carousel/"+postername}/>
+			);
+	},	3000);
+}*/
+	mixins: [NukaCarousel.ControllerMixin]
+	render() {
+  	return (
+			//{this.a()}
+	   	<NukaCarousel>
+	      <img className="carouselPoster" src={"/images/carousel/wonderwoman.jpg"}/>
+	      <img className="carouselPoster" src={"/images/carousel/War-For-The-Planet-Of-The-Apes.png"}/>
+	      <img className="carouselPoster" src={"/images/carousel/Spider-Man-Homecoming.jpg"}/>
+	      <img className="carouselPoster" src={"/images/carousel/Alien-Covenant.png"}/>
+	      <img className="carouselPoster" src={"/images/carousel/The-Mummy.jpg"}/>
+	    	<img className="carouselPoster" src={"/images/carousel/Finding-Dory.jpg"}/>
+	  	</NukaCarousel>
+
+  	);
 	}
 }
+
+//CinemoreCinemaProject/client/public
--- client/src/pages/listing/Listing.jsx
+++ client/src/pages/listing/Listing.jsx
@@ -20,7 +20,7 @@ export default class Listing extends Component {
 			showtimes: []
 		}
 	}
-	
+
 	componentDidMount() {
 		Client.fetchShowings(films => {
 			Client.fetchShowTimes(this.state.cinemaId, showtime => {
@@ -39,12 +39,12 @@ export default class Listing extends Component {
 			localStorage.filmTime = JSON.stringify(filmTime);
 			console.log(filmTitle + ": "+filmTime);
 	}
-	
+
 	displayFilms() {
 		return this.state.showings.map((film, idx) => {
 			return(
-				<FilmBlock key={idx} id={film.filmId} showtimeBlock={this.displayShowTimes(film.filmId, film.title)} 
-				title={film.title} genres={film.genres} posterFileName={film.poster} description={film.description} 
+				<FilmBlock key={idx} id={film.filmId} showtimeBlock={this.displayShowTimes(film.filmId, film.title)}
+				title={film.title} genres={film.genres} posterFileName={film.poster} description={film.description}
 				shortDes={film.shortDes} release={film.releaseDate} director={film.director} cast={film.cast} />
 			);
 		});
@@ -57,19 +57,19 @@ export default class Listing extends Component {
 			);
 		});
 	}
-	
+
 	displayLoading() {
 		return(
 			<div id="loading-icon"><img src="/images/loading.gif" alt="Loading..." /></div>
 		);
 	}
-	
-	
+
+
 	render(){ // removed <LocationSelect />
 		const { loading, showtimes } = this.state;
 		return(
 			<div className="page" id="filmShowings" >
-			
+
 				<div id="gridOfEquals" >
 					{ loading ? this.displayLoading() : this.displayFilms() }
 				</div>
