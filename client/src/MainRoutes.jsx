bc95e11fe591c132f9ffe3cd956e9642a6ffb8fd
James-Charlesworth <jcharlesworthwork@gmail.com>
2017-07-20 09:36:27 +0000

    mainRoutes accepts props and props down to all components

--- client/src/MainRoutes.jsx
+++ client/src/MainRoutes.jsx
@@ -3,40 +3,52 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from "./pages/home/Home";
 import Listing from './pages/listing/Listing';
+import Locations from './pages/locations/locations';
 import Contact from './pages/contact/Contact';
 import Faq from './pages/faq/Faq';
 import Booking from './pages/booking/Booking';
-import Cinema from './pages/about/Cinema';
 import BookNow from './components/BookNow';
 import Confirmation from './pages/confirmation/Confirmation';
-import Classifications from './pages/classifications/Classifications';
-import Formats from './pages/formats/Formats';
 
 export default class MainRoutes extends Component {
-	render() {
+	constructor(props) {
+		super(props);
+		this.handleChange = this.handleChange.bind(this);
+	}
+	
+	handleChange(e){
+		console.log(e);
+		this.props.onChange(e);
+	}
+	
+	render(props) {
+		const location = this.props.location;
+		const time = this.props.film;
+		const film = this.props.time;
 		return (
 		<div id="content">
 		  <main>
 			<Switch>
-			  <Route exact path='/' component={Home}/>
-			  <Route path="/booking" component={Booking}/>
-			  <Route path="/films" component={Listing}/>
+			  <Route exact path='/' component={() => <Home onChange={this.handleChange} location={location} film={film} time={time}/>} />
+			  <Route path="/booking" component={() => <Booking location={location} film={film} time={time}/>}/>
+			  <Route path="/films" component={() => <Listing location={location} film={film} time={time}/>}/>
+			  <Route path="/locations" component={() => <Locations location={location} film={film} time={time}/>}/>
 			  <Route path="/faq" component={Faq}/>
 			  <Route path="/contact" component={Contact}/>
-			  <Route path="/confirmation" component={Confirmation}/>
-			  <Route path="/cinema" component={Cinema}/>
+			  <Route path="/confirmation" component={() => <Confirmation location={location} film={film} time={time}/>}/>
+			<Route path="/cinema" component={Cinema}/>
 			  <Route path="/classifications" component={Classifications}/>
 			  <Route path="/formats" component={Formats}/>
-			  
 			</Switch>
 		  </main>
 		  <article>
 			<Switch>
-			  <Route exact path='/' component={BookNow}/>
-			  <Route path="/films" component={BookNow}/>
-			  <Route path="/faq" component={BookNow}/>
-			  <Route path="/contact" component={BookNow}/>
-			  <Route path="/cinema" component={BookNow}/>
+			  <Route exact path='/' component={() => <BookNow onChange={this.handleChange} location={location} film={film} time={time}/>}/>
+			  <Route path="/films" component={() => <BookNow onChange={this.handleChange} location={location} film={film} time={time}/>}/>
+			  <Route path="/locations" component={() => <BookNow onChange={this.handleChange} location={location} film={film} time={time}/>}/>
+			  <Route path="/faq" component={() => <BookNow onChange={this.handleChange} location={location} film={film} time={time}/>}/>
+			  <Route path="/contact" component={() => <BookNow onChange={this.handleChange} location={location} film={film} time={time}/>}/>
+				<Route path="/cinema" component={() => <BookNow onChange={this.handleChange} location={location} film={film} time={time}/>}/>
 			</Switch>
 		  </article>
 		</div>
