0f8388f9638d5e9628fb6c0968064ead1f18a68a
James-Charlesworth <jcharlesworthwork@gmail.com>
2017-07-20 09:48:47 +0000

    book now accepts props and can pass back up

--- client/src/components/BookNow.jsx
+++ client/src/components/BookNow.jsx
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class BookNow extends Component {
+	constructor(props){
+		super(props);
+		this.handleChange = this.handleChange.bind(this);
+	}
+	
+	handleChange(e){
+		//this.props.onChange("something");
+	}
+	
 	render() {
 		return(
 			<div id="bookNow">
 				<div className="left quickBook">
-				
+					<div className="left quickBook one">
+						<p>
+							onClick={this.handleChange} 
+							defaultValue={location} 
+						</p>
+					</div>
 				</div>
 				<div className="center quickBook">
 					<Link to={{ pathname: '/booking'}}>
