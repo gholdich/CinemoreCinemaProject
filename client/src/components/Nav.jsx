d95658159dfb777fe6257a77e568047edeb91c2f
James-Charlesworth <jcharlesworthwork@gmail.com>
2017-07-20 10:15:21 +0000

    nav handles props, passes between high state and lower components

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -5,12 +5,21 @@ import AccountPin from './AccountPin';
 import LocationLabel from './LocationLabel';
 
 export default class Nav extends Component {
+	constructor(props){
+		super(props);
+		this.handleChange = this.handleChange.bind(this);
+	}
+	
+	handleChange(e){
+		this.props.onChange(e);
+	}
+	
 	render() {
 		return(
 			<div id="navbar-container">
-				<LocationLabel />
+				<LocationLabel location={this.props.location} onChange={this.handleChange} />
 				<Navbar />
-				<LocationPin />
+				<LocationPin location={this.props.location} onChange={this.handleChange} />
 				<AccountPin />
 			</div>
 		);
