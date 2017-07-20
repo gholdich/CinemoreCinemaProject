bcd67010615b3c8018a2a2838474c4a851aada6b
Mallett94 <nmallett94@gmail.com>
2017-07-20 17:25:18 +0100

    classifications added to film page

--- client/src/pages/listing/Listing.jsx
+++ client/src/pages/listing/Listing.jsx
@@ -13,7 +13,7 @@ export default class Listing extends Component {
 		this._onChange = this._onChange.bind(this);
 		this._onLocationChange = this._onLocationChange.bind(this);
 		var selectedCinemaId = 0;
-		
+
 		if(localStorage.selectedCinemaId) {
 			selectedCinemaId = JSON.parse(localStorage.selectedCinemaId);
 		}
@@ -26,7 +26,7 @@ export default class Listing extends Component {
 			showtimes: [],
 			filterText: ""
 		}
-		
+
 		this.handleSearch = this.handleSearch.bind(this);
 	}
 
@@ -35,27 +35,27 @@ export default class Listing extends Component {
 		appStore.on("searchChange", this._onChange);
 		appStore.on("locationChange", this._onLocationChange);
 	}
-	
+
 	componentWillUnmount(){
 		//console.log("Unmount");
 		appStore.removeListener("searchChange", this._onChange);
 		appStore.removeListener("locationChange", this._onLocationChange);
 	}
-	
+
 	_onChange(){
 		this.setState({showings: appStore.getFilteredFilms()});
 	}
-	
+
 	_onLocationChange(){
 		var arr = appStore.getFilmsByLocation();
 		//console.log(arr);
-		
+
 		this.setState({filteredShowings: arr});
 		//console.log(this.state.showings);
 		//console.log("here");
 		//console.log(this.state.showings);
 	}
-	
+
 	componentDidMount() {
 		//console.log("Mounting");
 		Client.fetchShowings(films => {
@@ -67,11 +67,11 @@ export default class Listing extends Component {
 				});
 			});
 		});
-		
+
 		appActions.filterByLocation(this.props.location);
 
 	}
-	
+
 	//Here are the localStorage variables: selectedLocation; selectedCinemaId; filmTitle; filmTime;
 	updateLocalStorage_Book(filmTime, filmTitle) {
 			localStorage.filmTitle = JSON.stringify(filmTitle);
@@ -87,26 +87,26 @@ export default class Listing extends Component {
 		if(this.state.filterText != ""){
 			return this.state.showings.map((film, idx) => {
 				return(
-					<FilmBlock key={idx} id={film.filmId} showtimeBlock={this.displayShowTimes(film.filmId, film.title)} 
-					title={film.title} genres={film.genres} posterFileName={film.poster} description={film.description} 
-					shortDes={film.shortDes} release={film.releaseDate} director={film.director} cast={film.cast} videoId={film.videoId}/>
+					<FilmBlock key={idx} id={film.filmId} showtimeBlock={this.displayShowTimes(film.filmId, film.title)}
+					title={film.title} genres={film.genres} posterFileName={film.poster} description={film.description}
+					shortDes={film.shortDes} release={film.releaseDate} director={film.director} cast={film.cast} videoId={film.videoId} classification={film.classification} />
 				);
 			});
 		}else if(this.state.filteredShowings != ""){
 			return this.state.filteredShowings.map((film, idx) => {
 				return(
-					<FilmBlock key={idx} id={film.filmId} showtimeBlock={this.displayShowTimes(film.filmId, film.title)} 
-					title={film.title} genres={film.genres} posterFileName={film.poster} description={film.description} 
-					shortDes={film.shortDes} release={film.releaseDate} director={film.director} cast={film.cast} videoId={film.videoId}/>
+					<FilmBlock key={idx} id={film.filmId} showtimeBlock={this.displayShowTimes(film.filmId, film.title)}
+					title={film.title} genres={film.genres} posterFileName={film.poster} description={film.description}
+					shortDes={film.shortDes} release={film.releaseDate} director={film.director} cast={film.cast} videoId={film.videoId} classification={film.classification}/>
 				);
 			});
-			
+
 			}else{
 				return this.state.showings.map((film, idx) => {
 					return(
-						<FilmBlock key={idx} id={film.filmId} 
-						title={film.title} genres={film.genres} posterFileName={film.poster} description={film.description} 
-						shortDes={film.shortDes} release={film.releaseDate} director={film.director} cast={film.cast} videoId={film.videoId}/>
+						<FilmBlock key={idx} id={film.filmId}
+						title={film.title} genres={film.genres} posterFileName={film.poster} description={film.description}
+						shortDes={film.shortDes} release={film.releaseDate} director={film.director} cast={film.cast} videoId={film.videoId} classification={film.classification}/>
 					);
 				});
 			}
@@ -125,7 +125,7 @@ export default class Listing extends Component {
 			<div id="loading-icon"><img src="/images/loading.gif" alt="Loading..." /></div>
 		);
 	}
-	
+
 	handleSearch(filterText){
 		this.setState({filterText: filterText});
 		appActions.filterBySearch(filterText.toUpperCase());
@@ -137,9 +137,9 @@ export default class Listing extends Component {
 		return(
 			<div>
 				<MovieSearch filterText={this.state.filterText} onSearch={this.handleSearch.bind(this)} />
-					
+
 				<div className="page" id="filmShowings" >
-				
+
 					<div id="gridOfEquals" >
 						{ loading ? this.displayLoading() : this.displayFilms() }
 					</div>
